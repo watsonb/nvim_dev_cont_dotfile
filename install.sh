599763db4d84c73e6682576d7e202e68cba4358c
Ben Watson <benjamin.watson@kiewit.com>
2026-02-13 06:31:55 -0600

    fix: comment out apt stuff

--- install.sh
+++ install.sh
@@ -11,26 +11,26 @@ source "$DOTFILES_DIR/scripts/utils.sh"
 
 print_details
 
-apt_update
-apt_install \
-  fzf \
-  golang-go \
-  npm \
-  pass \
-  python3-venv \
-  software-properties-common \
-  stow \
-  wget \
-  unzip \
-  zsh
-
-# change to zsh as default shell
-sudo chsh -s /usr/bin/zsh
-
-# ensure that .config is owned by the current user
-if [[ -d $HOME/.config && ! $(stat -c "%U" "$HOME/.config") == "$(whoami)" ]]; then
-  sudo chown $(id -u):$(id -g) "$HOME/.config"
-fi
+# apt_update
+# apt_install \
+#   fzf \
+#   golang-go \
+#   npm \
+#   pass \
+#   python3-venv \
+#   software-properties-common \
+#   stow \
+#   wget \
+#   unzip \
+#   zsh
+#
+# # change to zsh as default shell
+# sudo chsh -s /usr/bin/zsh
+#
+# # ensure that .config is owned by the current user
+# if [[ -d $HOME/.config && ! $(stat -c "%U" "$HOME/.config") == "$(whoami)" ]]; then
+#   sudo chown $(id -u):$(id -g) "$HOME/.config"
+# fi
 
 # INFO "Installing NEOVIM..."
 # apt_install libfuse2 fuse3
